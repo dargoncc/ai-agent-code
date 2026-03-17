b25d082f9f07cbbeab677a73fce91065d2180d5d
shul <shulongcc@163.com>
2026-03-17 19:18:21 +0800

    feat: mini-cursor 创建

--- tool-test/src/hello-langchain.mjs
+++ tool-test/src/hello-langchain.mjs
@@ -5,7 +5,7 @@ dotenv.config()
 
 const model = new ChatOpenAI({
     modelName: process.env.MODEL_NAME || "qwen-coder-turbo",
-    apiKey: process.env.OPENAI_API_KET,
+    apiKey: process.env.OPENAI_API_KEY,
     configuration: {
         baseURL: process.env.OPENAI_BASE_URL
     }
